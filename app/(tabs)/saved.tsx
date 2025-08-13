e4a76edf4ee38b586669648690aa4e70babdab31
Sammy Kariuki <sammykariuki099@gmail.com>
2025-08-13 16:54:22 +0300

    initial commit

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Saved() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-lg font-bold text-blue-500">Saved</Text>
+    </View>
+  );
+}
---
+++ app/movie/[id].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function MovieDetails() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-lg font-bold text-blue-500">Movie Details</Text>
+    </View>
+  );
+}
